47e3d0cb1b54ce117346e0eb24af1ddd3119d17b
zhangneng <1037930435@qq.com>
2017-06-17 19:52:37 +0800

    process_view(self, request, view_func, view_func_agrs, view_func_kwargs):

--- Middle/m1.py
+++ Middle/m1.py
@@ -7,14 +7,19 @@ class Row1(MiddlewareMixin):
     def process_request(self,request):
         print "请求经过第1个中间件"
 
+    def process_view(self, request, view_func, view_func_agrs, view_func_kwargs):
+        print "请求返还之前的中间件1"
+
     def process_response(self, request,response):
-        print "请求返还第3个中间件"
+        print "请求返还第1个中间件"
         return response
+
 
 class Row2(MiddlewareMixin):
     def process_request(self,request):
         print "请求经过第2个中间件"
-
+    def process_view(self, request, view_func, view_func_agrs, view_func_kwargs):
+        print "请求返还之前的中间件2"
     def process_response(self,requset,response):
         print "请求返还第2个中间件"
         return response
@@ -23,7 +28,8 @@ class Row2(MiddlewareMixin):
 class Row3(MiddlewareMixin):
     def process_request(self,request):
         print "请求经过第3个中间件"
-
+    def process_view(self, request, view_func, view_func_agrs, view_func_kwargs):
+        print "请求返还之前的中间件3"
     def process_response(self,requset,response):
-        print "请求返还第1个中间件"
+        print "请求返还第3个中间件"
         return response

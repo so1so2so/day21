b5228005156621c25b8c70e363e6c2933910e10d
so1so2so <1037930435@qq.com>
2017-06-18 16:58:17 +0800

    rm test

---
+++ sg.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python
+# _*_ coding:utf-8 _*_
+from django.core.signals import request_started
+
+
+def callback(sender, **kwargs):
+        print("zhang_callback")
+        # print(sender, kwargs)
+request_started.connect(callback)
